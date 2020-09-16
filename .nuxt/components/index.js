7b406028151cdfd0453ffb1428a2e221273364b7
azuic <zuic@Zuis-MacBook-Pro.local>
2020-09-16 16:13:40 -0400

    updata location info for institutes

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,25 +1,13 @@
 export { default as AppHeader } from '../../components/AppHeader.vue'
 export { default as Canvas } from '../../components/Canvas.vue'
 export { default as Menu } from '../../components/Menu.vue'
-<<<<<<< HEAD
-export { default as NaviBar } from '../../components/NaviBar.vue'
-export { default as SubNavi } from '../../components/SubNavi.vue'
-export { default as WaffleChart } from '../../components/WaffleChart.vue'
-=======
 export { default as MenuSection } from '../../components/MenuSection.vue'
 export { default as NaviBar } from '../../components/NaviBar.vue'
 export { default as SubNavi } from '../../components/SubNavi.vue'
->>>>>>> origin/master
 
 export const LazyAppHeader = import('../../components/AppHeader.vue' /* webpackChunkName: "components/AppHeader" */).then(c => c.default || c)
 export const LazyCanvas = import('../../components/Canvas.vue' /* webpackChunkName: "components/Canvas" */).then(c => c.default || c)
 export const LazyMenu = import('../../components/Menu.vue' /* webpackChunkName: "components/Menu" */).then(c => c.default || c)
-<<<<<<< HEAD
-export const LazyNaviBar = import('../../components/NaviBar.vue' /* webpackChunkName: "components/NaviBar" */).then(c => c.default || c)
-export const LazySubNavi = import('../../components/SubNavi.vue' /* webpackChunkName: "components/SubNavi" */).then(c => c.default || c)
-export const LazyWaffleChart = import('../../components/WaffleChart.vue' /* webpackChunkName: "components/WaffleChart" */).then(c => c.default || c)
-=======
 export const LazyMenuSection = import('../../components/MenuSection.vue' /* webpackChunkName: "components/MenuSection" */).then(c => c.default || c)
 export const LazyNaviBar = import('../../components/NaviBar.vue' /* webpackChunkName: "components/NaviBar" */).then(c => c.default || c)
 export const LazySubNavi = import('../../components/SubNavi.vue' /* webpackChunkName: "components/SubNavi" */).then(c => c.default || c)
->>>>>>> origin/master
--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,15 +3,6 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-<<<<<<< HEAD
-const _154df549 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
-const _74289bcb = () => interopDefault(import('../pages/artists/index.vue' /* webpackChunkName: "pages/artists/index" */))
-const _9a599608 = () => interopDefault(import('../pages/company/index.vue' /* webpackChunkName: "pages/company/index" */))
-const _234b4e6f = () => interopDefault(import('../pages/database.vue' /* webpackChunkName: "pages/database" */))
-const _53561e77 = () => interopDefault(import('../pages/institution/index.vue' /* webpackChunkName: "pages/institution/index" */))
-const _61bb911f = () => interopDefault(import('../pages/artists/_artist.vue' /* webpackChunkName: "pages/artists/_artist" */))
-const _3505d00e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
-=======
 const _161ef372 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
 const _4a1160f4 = () => interopDefault(import('../pages/artists/index.vue' /* webpackChunkName: "pages/artists/index" */))
 const _ee880bb6 = () => interopDefault(import('../pages/company/index.vue' /* webpackChunkName: "pages/company/index" */))
@@ -22,7 +13,6 @@ const _59207fee = () => interopDefault(import('../pages/artist/_artist.vue' /* w
 const _6087b008 = () => interopDefault(import('../pages/artists/_artist.vue' /* webpackChunkName: "pages/artists/_artist" */))
 const _0808749b = () => interopDefault(import('../pages/institution/_institution.vue' /* webpackChunkName: "pages/institution/_institution" */))
 const _35d6ce37 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
->>>>>>> origin/master
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -42,33 +32,6 @@ export const routerOptions = {
 
   routes: [{
     path: "/about",
-<<<<<<< HEAD
-    component: _154df549,
-    name: "about"
-  }, {
-    path: "/artists",
-    component: _74289bcb,
-    name: "artists"
-  }, {
-    path: "/company",
-    component: _9a599608,
-    name: "company"
-  }, {
-    path: "/database",
-    component: _234b4e6f,
-    name: "database"
-  }, {
-    path: "/institution",
-    component: _53561e77,
-    name: "institution"
-  }, {
-    path: "/artists/:artist",
-    component: _61bb911f,
-    name: "artists-artist"
-  }, {
-    path: "/",
-    component: _3505d00e,
-=======
     component: _161ef372,
     name: "about"
   }, {
@@ -106,7 +69,6 @@ export const routerOptions = {
   }, {
     path: "/",
     component: _35d6ce37,
->>>>>>> origin/master
     name: "index"
   }],
 
